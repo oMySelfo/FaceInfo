d294571e740047e8ff183adb7f9d964dbf516874
jays1234 <02584561397a>
2014-11-21 15:15:44 +0700

    Add Edittext

---
+++ src/widget/Createtextview.java
@@ -0,0 +1,31 @@
+package widget;
+
+import android.content.Context;
+import android.view.ViewDebug.CapturedViewProperty;
+import android.widget.TextView;
+
+public class Createtextview extends TextView{
+
+	public Createtextview(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	@CapturedViewProperty
+	public int getId() {
+		// TODO Auto-generated method stub
+		return super.getId();
+	}
+
+	@Override
+	public void setId(int id) {
+		// TODO Auto-generated method stub
+		super.setId(id);
+	}
+
+	
+	
+	
+
+}
